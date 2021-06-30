9a1d249e36c875fbc2874aab8e7f7143405abb70
waiting <1661926154@qq.com>
2021-06-30 10:10:58 +0800

    ci(taskman): add tb_task_msg.sql

--- packages/taskman/src/database/init-db.sh
+++ packages/taskman/src/database/init-db.sh
@@ -8,6 +8,7 @@ psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U$POSTGRES_USER -d $POSTGRES_DB -bq \
   -f ddl/enum.sql \
   -f ddl/common.sql \
   -f ddl/tb_task.sql \
+  -f ddl/tb_task_msg.sql \
   -f ddl/tb_task_payload.sql \
   -f ddl/tb_task_progress.sql \
   -f ddl/tb_task_archive.sql \
