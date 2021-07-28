00f7892ec244cd30a957b13b34afdb84dbe2e044
张建洋 <zhangjianyang@graspyun.com>
2021-07-28 15:58:14 +0800

    feat(jaeger): 任务分发器span

--- packages/jaeger/src/index.ts
+++ packages/jaeger/src/index.ts
@@ -5,7 +5,9 @@ export { AutoConfiguration as Configuration } from './configuration'
 export * from './lib/index'
 export { TracerMiddleware } from './middleware/tracer.middleware'
 export { TracerExtMiddleware } from './middleware/tracer-ext.middleware'
-
+export {
+  globalTracer, Span,
+} from 'opentracing'
 
 declare module '@midwayjs/core' {
   interface Context {
