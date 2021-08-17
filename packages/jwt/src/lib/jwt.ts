66e56dea25270ed1a49f7fcf1fec652be6c2ff62
waiting <1661926154@qq.com>
2021-08-17 22:35:22 +0800

    chore: rename

--- packages/jwt/src/lib/jwt.ts
+++ packages/jwt/src/lib/jwt.ts
@@ -189,8 +189,8 @@ export class Jwt {
       return new Set([ctxSecret])
     }
 
-    const signSet = processSecret(this.config.secret)
-    const verifySet = processSecret(this.config.verifySecret)
+    const signSet = parseSecret(this.config.secret)
+    const verifySet = parseSecret(this.config.verifySecret)
     const ret = new Set([...verifySet, ...signSet])
 
     return ret
@@ -200,7 +200,7 @@ export class Jwt {
 }
 
 
-function processSecret(input?: JwtConfig['secret'] | JwtConfig['verifySecret']): Set<VerifySecret> {
+function parseSecret(input?: JwtConfig['secret'] | JwtConfig['verifySecret']): Set<VerifySecret> {
   const ret: Set<VerifySecret> = new Set()
 
   /* istanbul ignore else */
