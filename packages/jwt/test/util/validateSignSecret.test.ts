f414a4c441c11f0baf506de83c547bdd37b2da6b
waiting <1661926154@qq.com>
2021-08-18 13:58:37 +0800

    test: up

--- packages/jwt/test/util/validateSignSecret.test.ts
+++ packages/jwt/test/util/validateSignSecret.test.ts
@@ -2,7 +2,7 @@ import { relative } from 'path'
 
 import { token1 } from '../test.config'
 
-import { validateVerifySecret } from '~/index'
+import { validateSignSecret } from '~/index'
 
 // eslint-disable-next-line import/order
 import assert = require('power-assert')
@@ -12,29 +12,32 @@ const filename = relative(process.cwd(), __filename).replace(/\\/ug, '/')
 
 describe(filename, () => {
 
-  describe('Should validateVerifySecret() work', () => {
+  describe('Should validateSignSecret() work', () => {
     it('with valid input', () => {
       const arr = [
         'abc', '\n', token1,
+        { key: 'foo', passphrase: 'bar' },
         Buffer.from('foo'),
-      ] as const
+      ]
 
-      arr.forEach(validateVerifySecret)
+      arr.forEach(validateSignSecret)
     })
 
     it('with invalid input', () => {
       const arr = [
-        '', Buffer.alloc(0), {},
-        true, null, void 0, Symbol('foo'),
+        '', Buffer.alloc(0),
+        {}, { foo: 'bar' },
+        { key: 123 },
+        { passphrase: 123 },
+        true, false, null, void 0, Symbol('foo'),
         // eslint-disable-next-line @typescript-eslint/no-empty-function
-        () => { },
-        false,
+        () => {},
       ]
 
       arr.forEach((val) => {
         try {
           // @ts-ignore
-          validateVerifySecret(val)
+          validateSignSecret(val)
         }
         catch (ex) {
           return assert(true)
