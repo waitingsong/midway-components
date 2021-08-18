71ad693506151f001fd78ca34d7dcf2398ed4df8
waiting <1661926154@qq.com>
2021-08-18 13:50:44 +0800

    test: add resolver.test.ts

---
+++ packages/jwt/test/util/resolver.test.ts
@@ -0,0 +1,88 @@
+import { relative } from 'path'
+
+import { token1 } from '../test.config'
+
+import {
+  schemePrefix,
+  resolveFromAuthorizationHeader,
+  resolveFromCookies,
+} from '~/index'
+
+// eslint-disable-next-line import/order
+import assert = require('power-assert')
+
+
+const filename = relative(process.cwd(), __filename).replace(/\\/ug, '/')
+
+describe(filename, () => {
+
+  describe('Should resolveFromAuthorizationHeader() works', () => {
+    it('with valid input', () => {
+      const arr = [
+        `${schemePrefix} ${token1}`,
+        `${schemePrefix}   ${token1}`,
+      ]
+
+      arr.forEach((val) => {
+        const token = resolveFromAuthorizationHeader(val)
+        assert(token && token === token1)
+      })
+    })
+
+    it('with invalid type input', () => {
+      const arr = [
+        '',
+        1,
+        true,
+        false,
+        null,
+        void 0,
+      ]
+
+      arr.forEach((val) => {
+        // @ts-ignore
+        const token = resolveFromAuthorizationHeader(val)
+        assert(token === '')
+      })
+    })
+
+    it('with invalid header input', () => {
+      const arr = [
+        ` ${schemePrefix}   ${token1}`,
+        ` ${schemePrefix} ${token1}`,
+        `${schemePrefix} ${token1}  `,
+        `${schemePrefix} ${token1} `,
+        `${schemePrefix} ${token1} fake`,
+        `${schemePrefix}: ${token1}`,
+        `${schemePrefix} `,
+        `${token1}`,
+        ` ${token1}`,
+      ]
+
+      arr.forEach((val) => {
+        const token = resolveFromAuthorizationHeader(val)
+        assert(token === '')
+      })
+    })
+  })
+
+
+  describe('Should resolveFromCookies() works', () => {
+    it('with valid cookieKey', () => {
+      const cookies = new Map<string, string>()
+      cookies.set('user', token1)
+      // @ts-ignore
+      const ret = resolveFromCookies(cookies, 'user')
+      assert(ret === token1)
+    })
+
+    it('with blank cookieKey', () => {
+      const cookies = new Map<string, string>()
+      cookies.set('user', token1)
+      // @ts-ignore
+      const ret = resolveFromCookies(cookies, '')
+      assert(ret === '')
+    })
+  })
+})
+
