191d402d1fdee5e7a7d67f1dbc5c89a32dbc7f8f
waiting <1661926154@qq.com>
2021-08-19 11:58:55 +0800

    chore: up

--- packages/jwt/src/lib/types.ts
+++ packages/jwt/src/lib/types.ts
@@ -43,7 +43,7 @@ export interface JwtMiddlewareConfig extends JwtAuthenticateOptions {
    */
   ignore?: MiddlewarePathPattern
   /**
-   * Ignored if authOpts.passthrought true
+   * Ignored if JwtAuthenticateOptions.passthrought true
    * @default false
    */
   debug: boolean
