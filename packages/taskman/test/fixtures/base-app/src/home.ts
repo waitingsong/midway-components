3c94de488cd0735dd1c5474a47e602a3ea5dc18e
waiting <1661926154@qq.com>
2022-02-18 18:06:58 +0800

    test(taskman): add files

---
+++ packages/taskman/test/fixtures/base-app/src/home.ts
@@ -0,0 +1,34 @@
+/* eslint-disable node/no-unpublished-import */
+import {
+  Controller,
+  Get,
+} from '@midwayjs/decorator'
+
+import { Context } from '../../../../src/index'
+import { TestRespBody } from '../../../root.config'
+
+
+@Controller('/')
+export class HomeController {
+
+  // @Inject() readonly ctx: Context
+
+  @Get('/')
+  async home(): Promise<TestRespBody> {
+    // @ts-expect-error
+    const { jwtState, cookies, header, url } = this._req_ctx as Context
+    const res = {
+      jwtState,
+      cookies,
+      header,
+      url,
+      jwtOriginalErrorText: '',
+    }
+    if (jwtState.jwtOriginalError) {
+      res.jwtOriginalErrorText = jwtState.jwtOriginalError.message
+    }
+    return res
+  }
+
+}
+
