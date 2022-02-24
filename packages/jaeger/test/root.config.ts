67cd248267c1d9fbd3202e1b495638ae44fad0f3
waiting <1661926154@qq.com>
2022-02-24 14:07:34 +0800

    test(jaeger): refactor for midway.js v3

--- packages/jaeger/test/root.config.ts
+++ packages/jaeger/test/root.config.ts
@@ -1,3 +1,4 @@
+import { IncomingHttpHeaders } from 'http'
 import supertest, { SuperTest } from 'supertest'
 
 import { Application } from '~/interface'
@@ -7,14 +8,15 @@ import { TestSpanInfo } from '~/lib/types'
 export type TestResponse = supertest.Response
 export interface TestRespBody {
   cookies: unknown
-  header: unknown
   url: string
+  header: IncomingHttpHeaders
   spanInfo: TestSpanInfo
 }
 
 export interface TestConfig {
   app: Application
   httpRequest: SuperTest<supertest.Test>
+  host: string
 }
 export const testConfig = {
 } as TestConfig
