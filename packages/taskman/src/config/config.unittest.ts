79cfb390c358b8aaa144e27b21f83aad113545f2
waiting <1661926154@qq.com>
2022-03-08 23:25:10 +0800

    ci: add config.unittest.ts

---
+++ packages/taskman/src/config/config.unittest.ts
@@ -0,0 +1,31 @@
+import {
+  initTaskManClientConfig,
+  initDbConfig,
+  TaskManClientConfig,
+  TaskManServerConfig,
+  TaskAgentConfig,
+  initTaskAgentConfig,
+} from '../lib/index'
+
+
+/**
+ * Remove this variable if running as client
+ */
+export const taskManServerConfig: TaskManServerConfig = {
+  expInterval: '30min',
+  dbConfigs: {
+    ...initDbConfig,
+    enableTracing: false,
+  },
+  headerKey: 'x-task-agent',
+  headerKeyTaskId: 'x-task-id',
+}
+
+export const taskManClientConfig: TaskManClientConfig = {
+  ...initTaskManClientConfig,
+}
+
+export const taskAgentConfig: TaskAgentConfig = {
+  ...initTaskAgentConfig,
+}
+
