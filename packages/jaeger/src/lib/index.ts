ff04b221516944dec0c8f459f1dc097c04256c21
waiting <1661926154@qq.com>
2022-03-10 22:03:24 +0800

    chore: 7

--- packages/jaeger/src/lib/index.ts
+++ packages/jaeger/src/lib/index.ts
@@ -1,6 +1,6 @@
 
-export { Logger } from './logger'
-export { TracerManager } from './tracer'
 export * from './config'
 export * from './types'
+export { Logger } from './logger'
+export { TracerManager } from './tracer'
 
