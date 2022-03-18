1832d86fddb423f6aa3135635551e9a15b78ea35
waiting <1661926154@qq.com>
2022-03-18 16:43:14 +0800

    chore: up

--- packages/jwt/src/lib/index.ts
+++ packages/jwt/src/lib/index.ts
@@ -1,8 +1,7 @@
 
 export * from './config'
 export * from './types'
-export { JwtComponent } from './component'
-export { Jwt } from './jwt'
 export * from './resolvers'
 export * from './util'
+export { JwtComponent } from './component'
 
