2d988502ee0556bfe2a70514ebc3c47a08de0dc5
waiting <1661926154@qq.com>
2022-03-18 15:14:34 +0800

    test: up

--- packages/taskman/test/app/task-agent-controller/20.stop.test.ts
+++ packages/taskman/test/app/task-agent-controller/20.stop.test.ts
@@ -17,6 +17,13 @@ describe(filename, () => {
       assert(tm.runningTasks.size === 0)
       assert(tm.runningTasks.size <= taskClientConfig.maxRunner)
 
+      void httpRequest
+        .get(`${ClientURL.base}/${ClientURL.start}`)
+        .expect(200)
+      void httpRequest
+        .get(`${ClientURL.base}/${ClientURL.start}`)
+        .expect(200)
+
       await httpRequest
         .get(`${ClientURL.base}/${ClientURL.stop}/fakeid`)
         .expect(200)
@@ -26,11 +33,18 @@ describe(filename, () => {
         .expect(200)
 
       const { count } = resp.body as TaskAgentState
-      assert(count === 1)
+      assert(count === 2)
     })
 
     it('effect with valid agentId', async () => {
       const { httpRequest } = testConfig
+
+      void httpRequest
+        .get(`${ClientURL.base}/${ClientURL.start}`)
+        .expect(200)
+      void httpRequest
+        .get(`${ClientURL.base}/${ClientURL.start}`)
+        .expect(200)
 
       const resp = await httpRequest
         .get(`${ClientURL.base}/${ClientURL.status}`)
