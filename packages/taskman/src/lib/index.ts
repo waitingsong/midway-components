0bc9641a327754340d5d76229b24d26467fac769
waiting <1661926154@qq.com>
2022-03-18 15:22:04 +0800

    refactor: file pos

--- packages/taskman/src/lib/index.ts
+++ packages/taskman/src/lib/index.ts
@@ -5,5 +5,4 @@ export * from './database.do'
 export * from './db.model'
 export * from './tm.dto'
 export * from './types'
-export { ClientService } from './client.service'
 
--- packages/taskman/src/service/index.service.ts
+++ packages/taskman/src/service/index.service.ts
@@ -1,3 +1,5 @@
 
+export { ClientService } from './client.service'
 export { TaskAgentService } from './task-agent.service'
 export { TaskQueueService } from './task-queue.service'
+
