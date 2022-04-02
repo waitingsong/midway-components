4a0571813e715d17fb8ff0cc2de7756ad251584e
waiting <1661926154@qq.com>
2022-04-02 15:36:55 +0800

    test(jaeger): fix test/config.unittest.ts

--- packages/jaeger/test/config.unittest.ts
+++ packages/jaeger/test/config.unittest.ts
@@ -6,10 +6,22 @@ import {
   MiddlewareOptions,
   initialConfig,
 } from '~/index'
+import { processCustomFailure } from '~/lib/tracer'
 
 
 export const config: Config = {
   ...initialConfig,
+  processCustomFailure,
+  tracingConfig: {
+    // serviceName: 'jaeger',
+    sampler: {
+      type: 'probabilistic',
+      param: 1,
+    },
+    reporter: {
+      agentHost: '127.0.0.1',
+    },
+  },
 }
 
 export const mwConfig: Readonly<Omit<MiddlewareConfig, 'match'>> = {
