9170fd3fed0823a41faf977f5e990ad03d6cb4fd
waiting <1661926154@qq.com>
2022-04-02 15:49:58 +0800

    chore(jwt): update types merging

--- packages/jwt/src/index.ts
+++ packages/jwt/src/index.ts
@@ -1,3 +1,6 @@
+// @ts-ignore
+import { PowerPartial } from '@midwayjs/core'
+
 import {
   Config,
   ConfigKey,
@@ -20,9 +23,9 @@ export {
 } from 'jsonwebtoken'
 
 
-declare module '@midwayjs/core' {
+declare module '@midwayjs/core/dist/interface' {
   interface MidwayConfig {
-    [ConfigKey.config]: Config
-    [ConfigKey.middlewareConfig]: MiddlewareConfig
+    [ConfigKey.config]: PowerPartial<Config>
+    [ConfigKey.middlewareConfig]: PowerPartial<MiddlewareConfig>
   }
 }
