df76836b5ceef488de3ad4fc30137847b9ce03a8
waiting <1661926154@qq.com>
2022-06-04 14:13:13 +0800

    test(ali-oss): update with ClientKey

--- packages/ali-oss/test/component/14.createSymlink.test.ts
+++ packages/ali-oss/test/component/14.createSymlink.test.ts
@@ -2,6 +2,7 @@ import assert from 'node:assert/strict'
 import { relative } from 'node:path'
 
 import { cloudUrlPrefix, testConfig, src } from '@/root.config'
+import { ClientKey } from '~/index'
 
 
 const filename = relative(process.cwd(), __filename).replace(/\\/ug, '/')
@@ -16,7 +17,7 @@ describe(filename, () => {
       await ossClient.cp('master', src, target)
 
       const link = `${target}-link`
-      const ret = await ossClient.createSymlink('master', target, link)
+      const ret = await ossClient.createSymlink(ClientKey.master, target, link)
       CI || console.log(ret)
       assert(ret.exitCode === 0)
       assert(ret.data)
