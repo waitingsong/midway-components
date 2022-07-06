4463096ae37e3549b772fb6e06f31a52c007cc3d
waiting <1661926154@qq.com>
2022-07-07 00:24:50 +0800

    chore(jaeger): update declaration of MidwayConfig of @midwayjs/core/dist/interface
    use Partial instead of PowerPartial

--- packages/jaeger/src/index.ts
+++ packages/jaeger/src/index.ts
@@ -1,5 +1,3 @@
-// @ts-ignore
-import { PowerPartial } from '@midwayjs/core'
 
 import {
   Config,
@@ -21,8 +19,8 @@ export {
 // @ts-ignore
 declare module '@midwayjs/core/dist/interface' {
   interface MidwayConfig {
-    [ConfigKey.config]: PowerPartial<Config>
-    [ConfigKey.middlewareConfig]: PowerPartial<MiddlewareConfig>
+    [ConfigKey.config]: Partial<Config>
+    [ConfigKey.middlewareConfig]: Partial<MiddlewareConfig>
   }
 
   interface Context {
