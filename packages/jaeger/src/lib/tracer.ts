7430d5ea61442be5ae579e8ac5c6454ddf7514a7
waiting <1661926154@qq.com>
2022-08-10 16:15:35 +0800

    chore(jaeger): lint fix

--- packages/jaeger/src/lib/tracer.ts
+++ packages/jaeger/src/lib/tracer.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
 import assert from 'assert'
 import type { IncomingHttpHeaders } from 'http'
 
