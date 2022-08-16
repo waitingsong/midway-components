ae5bdf57dbbfb8a7116d580cf348b660835e6edd
waiting <1661926154@qq.com>
2022-08-16 17:39:16 +0800

    feat(share): export types

--- packages/share/src/index.ts
+++ packages/share/src/index.ts
@@ -1,4 +1,35 @@
+import { MidwayConfig } from '@midwayjs/core'
+import type { PrometheusConfig } from '@midwayjs/prometheus'
+
 
 export * from './lib/middleware.js'
 export * from './lib/my-error.js'
+
+
+// @ts-ignore
+declare module '@midwayjs/core/dist/interface' {
+  interface MidwayConfig {
+    prometheus?: PrometheusConfig
+  }
+}
+// @ts-ignore
+declare module '@midwayjs/koa/dist/interface' {
+  interface Context {
+    reqId: string
+    _internalError?: Error
+  }
+}
+
+export {
+  IMidwayApplication,
+  IMidwayContainer,
+  IMiddleware,
+  NextFunction,
+} from '@midwayjs/core'
+
+export type AppConfig = Partial<MidwayConfig>
+export {
+  Application,
+  Context,
+} from '@midwayjs/koa'
 
