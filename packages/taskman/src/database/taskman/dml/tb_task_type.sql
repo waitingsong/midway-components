7b47cbbcab2765891f43a4e08ef92b83954a5f9b
waiting <1661926154@qq.com>
2022-09-22 16:44:41 +0800

    ci(taskman): fix tb_task_type.sql

--- packages/taskman/src/database/taskman/dml/tb_task_type.sql
+++ packages/taskman/src/database/taskman/dml/tb_task_type.sql
@@ -1,13 +1,13 @@
 --
 
-INSERT INTO tb_task_type VALUES (1, 'default')
-INSERT INTO tb_task_type VALUES (2, 'task 2')
-INSERT INTO tb_task_type VALUES (3, 'task 3')
-INSERT INTO tb_task_type VALUES (4, 'task 4')
-INSERT INTO tb_task_type VALUES (5, 'task 5')
-INSERT INTO tb_task_type VALUES (6, 'task 6')
-INSERT INTO tb_task_type VALUES (7, 'task 7')
-INSERT INTO tb_task_type VALUES (8, 'task 8')
-INSERT INTO tb_task_type VALUES (9, 'task 9')
+INSERT INTO tb_task_type VALUES (1, 'default');
+INSERT INTO tb_task_type VALUES (2, 'task 2');
+INSERT INTO tb_task_type VALUES (3, 'task 3');
+INSERT INTO tb_task_type VALUES (4, 'task 4');
+INSERT INTO tb_task_type VALUES (5, 'task 5');
+INSERT INTO tb_task_type VALUES (6, 'task 6');
+INSERT INTO tb_task_type VALUES (7, 'task 7');
+INSERT INTO tb_task_type VALUES (8, 'task 8');
+INSERT INTO tb_task_type VALUES (9, 'task 9');
 
 --
