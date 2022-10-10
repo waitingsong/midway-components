0629312e1cd6f2a046f81ed742313e97e4e9d910
waiting <1661926154@qq.com>
2022-10-11 02:00:59 +0800

    feat(base): use OpenTelemetry instead of jaeger client

--- packages/base/src/imports.ts
+++ packages/base/src/imports.ts
@@ -3,17 +3,17 @@ import * as prometheus from '@midwayjs/prometheus'
 import * as validate from '@midwayjs/validate'
 import * as aliOss from '@mwcp/ali-oss'
 import * as fetch from '@mwcp/fetch'
-import * as jaeger from '@mwcp/jaeger'
 import * as jwt from '@mwcp/jwt'
 import * as db from '@mwcp/kmore'
 import * as koid from '@mwcp/koid'
+import * as otel from '@mwcp/otel'
 // import { customLogger } from './util/custom-logger'
 
 
 export const useComponents: IComponentInfo[] = [
   koa,
   validate,
-  jaeger,
+  otel,
   prometheus,
   jwt,
   koid,
@@ -25,7 +25,7 @@ export const useComponents: IComponentInfo[] = [
 export const useDefaultRoutes: (string | RegExp)[] = [
   new RegExp(`/${aliOss.AliOssConfigKey.namespace}/.+`, 'u'),
   new RegExp(`/${db.ConfigKey.namespace}/.+`, 'u'),
-  new RegExp(`/${jaeger.TracerConfigKey.namespace}/.+`, 'u'),
+  new RegExp(`/${otel.OtelConfigKey.namespace}/.+`, 'u'),
   new RegExp(`/${koid.KoidConfigKey.namespace}/.+`, 'u'),
 ]
 
