cca030c376c572f4b10048d35344ce53e23513da
waiting <1661926154@qq.com>
2022-12-06 11:13:33 +0800

    feat(cache): define const classDecoratorKeyMap, methodDecoratorKeyMap
    [skip ci]

--- packages/cache/src/lib/config.ts
+++ packages/cache/src/lib/config.ts
@@ -23,4 +23,9 @@ export const initialMiddlewareConfig: Readonly<Omit<MiddlewareConfig, 'ignore' |
 export const CLASS_KEY_Cacheable = 'decorator:decorator_class_key_cacheable'
 export const METHOD_KEY_Cacheable = 'decorator:decorator_method_key_cacheable'
 export const METHOD_KEY_CacheEvict = 'decorator:decorator_method_key_cacheevict'
+export const classDecoratorKeyMap = new Map([ [CLASS_KEY_Cacheable, 'Cacheable'] ])
+export const methodDecoratorKeyMap = new Map([
+  [METHOD_KEY_Cacheable, 'Cacheable'],
+  [METHOD_KEY_CacheEvict, 'CacheEvict'],
+])
 
