ab72b53b4b9f055568cee627e823e18e0cd0ead5
waiting <1661926154@qq.com>
2023-02-27 17:25:38 +0800

    test(cache): update 90.helper.test.ts

--- packages/cache/test/helper/90.helper.test.ts
+++ packages/cache/test/helper/90.helper.test.ts
@@ -12,6 +12,7 @@ const filename = relative(process.cwd(), __filename).replace(/\\/ug, '/')
 describe(filename, () => {
   const key = 'CustService.getUserList:{"orderfield":"total","pageindex":1,"pagesize":5}'
   const expectHash = 'CustService.443bb521e2a0b756631224861f7a54f6'
+  const key2 = 'CustService.getUserList'
 
   describe('Should hashCacheKey() work', () => {
     it('common', async () => {
@@ -25,14 +26,13 @@ describe(filename, () => {
     })
 
     it('no hash', async () => {
-      const key2 = 'CustService.getUserList'
       const ret = hashCacheKey(key2)
       console.log({ ret })
       assert(ret.cacheKey === key2)
       assert(! ret.cacheKeyHash)
     })
 
-    it('save and read', async () => {
+    it('save and read for long key', async () => {
       const cacheManager = await testConfig.container.getAsync(CacheManager)
       assert(cacheManager)
 
@@ -49,6 +49,26 @@ describe(filename, () => {
 
       await deleteData(cacheManager, key)
       const ret3 = await getData(cacheManager, key)
+      assert(typeof ret3 === 'undefined')
+    })
+
+    it('save and read for short key', async () => {
+      const cacheManager = await testConfig.container.getAsync(CacheManager)
+      assert(cacheManager)
+
+      const ret = await saveData(cacheManager, key2, { a: 1 }, 30)
+      console.log({ ret })
+      assert(ret)
+      assert(ret.CacheMetaType)
+      assert(! ret.CacheMetaType.cacheKeyHash)
+
+      const ret2 = await getData(cacheManager, key2)
+      assert(ret2)
+      assert(ret2.CacheMetaType)
+      assert(! ret2.CacheMetaType.cacheKeyHash)
+
+      await deleteData(cacheManager, key2)
+      const ret3 = await getData(cacheManager, key2)
       assert(typeof ret3 === 'undefined')
     })
 
