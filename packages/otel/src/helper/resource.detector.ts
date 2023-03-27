9b82e28b30ecce7332a31b87579745cf39f37c86
waiting <1661926154@qq.com>
2023-03-27 17:18:59 +0800

    feat(otel): add more resource detectors
    - containerDetector
    - awsEksDetector
    - awsEc2Detector
    - gcpDetector

--- packages/otel/src/helper/resource.detector.ts
+++ packages/otel/src/helper/resource.detector.ts
@@ -1,4 +1,7 @@
 import { alibabaCloudEcsDetector } from '@opentelemetry/resource-detector-alibaba-cloud'
+import { awsEc2Detector, awsEksDetector } from '@opentelemetry/resource-detector-aws'
+import { containerDetector } from '@opentelemetry/resource-detector-container'
+import { gcpDetector } from '@opentelemetry/resource-detector-gcp'
 import {
   detectResourcesSync,
   envDetector,
@@ -10,11 +13,18 @@ import {
 
 export const detectorResources = detectResourcesSync({
   detectors: [
+    containerDetector,
     envDetector,
     hostDetector,
     osDetector,
     processDetector,
+
     alibabaCloudEcsDetector,
+    // Ordered AWS Resource Detectors as per:
+    // https://github.com/open-telemetry/opentelemetry-collector-contrib/blob/main/processor/resourcedetectionprocessor/README.md#ordering
+    awsEksDetector,
+    awsEc2Detector,
+    gcpDetector,
   ],
 })
 
