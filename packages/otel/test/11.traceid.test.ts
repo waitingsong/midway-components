b88e284ba0805e4c1ca3889c3d4baf4b172e1975
waiting <1661926154@qq.com>
2023-03-27 17:10:35 +0800

    test(otel): update test/11.traceid.test.ts

--- packages/otel/test/11.traceid.test.ts
+++ packages/otel/test/11.traceid.test.ts
@@ -145,7 +145,8 @@ describe(filename, () => {
 
     assert(
       span0.operationName === 'DefaultComponentService/hello'
-      || span0.operationName === 'DefaultComponentController/traceId2',
+      || span0.operationName === 'DefaultComponentController/traceId2'
+      || span0.operationName === '/_otel/id2',
       span0.operationName,
     )
 
