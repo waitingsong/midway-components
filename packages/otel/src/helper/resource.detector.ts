38582250aa036b7c9aa4360356370422cba31561
waiting <1661926154@qq.com>
2023-03-28 10:16:06 +0800

    chore(otel): clean dependencies

--- packages/otel/src/helper/resource.detector.ts
+++ packages/otel/src/helper/resource.detector.ts
@@ -2,29 +2,26 @@ import { alibabaCloudEcsDetector } from '@opentelemetry/resource-detector-alibab
 import { awsEc2Detector, awsEksDetector } from '@opentelemetry/resource-detector-aws'
 import { containerDetector } from '@opentelemetry/resource-detector-container'
 import { gcpDetector } from '@opentelemetry/resource-detector-gcp'
-import {
-  detectResourcesSync,
-  envDetector,
-  hostDetector,
-  osDetector,
-  processDetector,
-} from '@opentelemetry/resources'
+import { resources } from '@opentelemetry/sdk-node'
 
 
-export const detectorResources = detectResourcesSync({
-  detectors: [
-    containerDetector,
-    envDetector,
-    hostDetector,
-    osDetector,
-    processDetector,
+const detectors = [
+  containerDetector,
+  resources.envDetectorSync,
+  resources.hostDetectorSync,
+  resources.osDetectorSync,
+  resources.processDetectorSync,
 
-    alibabaCloudEcsDetector,
-    // Ordered AWS Resource Detectors as per:
-    // https://github.com/open-telemetry/opentelemetry-collector-contrib/blob/main/processor/resourcedetectionprocessor/README.md#ordering
-    awsEksDetector,
-    awsEc2Detector,
-    gcpDetector,
-  ],
+  alibabaCloudEcsDetector,
+  // Ordered AWS Resource Detectors as per:
+  // https://github.com/open-telemetry/opentelemetry-collector-contrib/blob/main/processor/resourcedetectionprocessor/README.md#ordering
+  awsEksDetector,
+  awsEc2Detector,
+  gcpDetector,
+]
+
+
+export const detectorResources = resources.detectResourcesSync({
+  detectors,
 })
 
