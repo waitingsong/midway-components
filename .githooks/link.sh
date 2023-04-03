7caea3170820ed02a214a53a652263ead4de7179
waiting <1661926154@qq.com>
2023-04-03 14:12:37 +0800

    ci: update link.sh

--- .githooks/link.sh
+++ .githooks/link.sh
@@ -2,9 +2,18 @@
 set -e
 pwd
 
+rm -rf "$(npm root -g)/@mwcp/otel"
+rm -rf node_modules/@mwcp/otel
+npm link ./packages/otel --no-audit
+
+rm -rf "$(npm root -g)/@mwcp/cache"
+rm -rf node_modules/@mwcp/cache
+npm link ./packages/cache --no-audit
+
 rm -rf "$(npm root -g)/@mwcp/share"
+rm -rf node_modules/@mwcp/share
 npm link ./packages/share --no-audit
 
-rm -rf "$(npm root -g)/@mwcp/otel"
-npm link ./packages/otel --no-audit
+ls -al node_modules/@mwcp
+
 
