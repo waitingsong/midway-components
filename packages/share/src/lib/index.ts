fb2ae7f89a75af3377aa890d0b652fe8e34cc77b
waiting <1661926154@qq.com>
2023-04-21 11:28:07 +0800

    ci: 1

--- packages/share/src/lib/index.ts
+++ packages/share/src/lib/index.ts
@@ -1,5 +1,5 @@
 
-export * from './decorator/custom-decorator.js'
+export * from './decorator/reg-decorator.js'
 export * from './decorator/custom-decorator.helper.js'
 export * from './decorator/custom-decorator.types.js'
 export * from './middleware.js'
