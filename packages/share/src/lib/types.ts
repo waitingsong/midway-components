ffb76743e1e79fe221911e1926f988a9abd0e98e
waiting <1661926154@qq.com>
2024-01-25 19:47:30 +0800

    feat(share): define type PagingResult<T>

--- packages/share/src/lib/types.ts
+++ packages/share/src/lib/types.ts
@@ -75,3 +75,25 @@ export interface BaseConfig {
 
 
 export type MiddlewareConfig<T> = MWConfig<T>
+
+/** 分页查询结果包装器 */
+export class PagingResult<T> {
+  /**
+   * Total count of rows in table
+   *
+   * @note This is the number of query response rows,
+   *  not the number of rows in the current page,
+   *  also not the number of pages.
+   */
+  total: number
+  /**
+   * Current page number, start from 1
+   */
+  page: number
+  /**
+   * Number of rows of each page.
+   * The number rows of the last page may be less than this value
+   */
+  pageSize: number
+  rows: T[]
+}
