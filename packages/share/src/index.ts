2d8f279ea98033d2121346b11d60ea2f2d0271b4
waiting <1661926154@qq.com>
2024-01-27 16:54:05 +0800

    chore(share): clean

--- packages/share/src/index.ts
+++ packages/share/src/index.ts
@@ -5,7 +5,6 @@ export { AutoConfiguration as Configuration } from './configuration.js'
 export * from './app/index.controller.js'
 export * from './lib/index.js'
 export * from './util/common.js'
-export * from './middleware/index.middleware.js'
 
 export { deepmerge }
 
