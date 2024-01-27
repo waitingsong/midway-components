ccc3558581083916a47ff6b4edbeca5cefeb0bd3
waiting <1661926154@qq.com>
2024-01-27 17:01:58 +0800

    chore(jwt): update code

--- packages/jwt/src/imports.ts
+++ packages/jwt/src/imports.ts
@@ -1,8 +1,7 @@
-// import * as info from '@midwayjs/info'
+import * as info from '@midwayjs/info'
 import * as koa from '@midwayjs/koa'
 import * as swagger from '@midwayjs/swagger'
 // import * as otel from '@mwcp/otel'
-// import * as share from '@mwcp/share'
 
 /* c8 ignore next 4 */
 const CI = !! (process.env['MIDWAY_SERVER_ENV'] === 'unittest'
@@ -13,9 +12,8 @@ const CI = !! (process.env['MIDWAY_SERVER_ENV'] === 'unittest'
 
 export const useComponents: IComponentInfo[] = []
 if (CI) {
-  // useComponents.push(share)
   useComponents.push(koa)
-  // useComponents.push(info)
+  useComponents.push(info)
   useComponents.push(swagger)
   // useComponents.push(otel) // ! use otel will cause CI fail if otel server not exists
 }
