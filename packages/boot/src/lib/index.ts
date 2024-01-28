82cb1f46c5fc1a120bab81e206e0577e20be0c52
waiting <1661926154@qq.com>
2024-01-28 19:04:51 +0800

    chore(boot): remove boot
    to https://github.com/waitingsong/midway-boot

--- packages/boot/src/lib/index.ts
+++
@@ -1,4 +0,0 @@
-
-export * from './types.js'
-export { ConfigKey as BootConfigKey } from './types.js'
-
--- packages/boot/src/middleware/index.middleware.ts
+++
@@ -1,6 +0,0 @@
-
-export * from './error-handler.middleware.js'
-export * from './json-response.middleware.js'
-export * from './request-id.middleware.js'
-export * from './response-headers.middleware.js'
-export * from './response-mime.middleware.js'
