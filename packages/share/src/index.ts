9fabdf0502293d1cad3504818a2d4921d55669c9
waiting <1661926154@qq.com>
2024-01-31 11:40:25 +0800

    feat(share): export functions
    - genAbsolutePath()
    - genCurrentDirname()
    - genCurrentFilename()
    - genISO8601String()

--- packages/share/src/index.ts
+++ packages/share/src/index.ts
@@ -6,6 +6,13 @@ export * from './app/index.controller.js'
 export * from './lib/index.js'
 export * from './util/common.js'
 
+export {
+  genAbsolutePath,
+  genCurrentDirname,
+  genCurrentFilename,
+  genISO8601String,
+} from '@waiting/shared-core'
+
 export { deepmerge }
 
 
