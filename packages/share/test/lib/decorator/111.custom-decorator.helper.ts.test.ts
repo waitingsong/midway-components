9225edeca42217888ebba60117c535253a94ffd5
waiting <1661926154@qq.com>
2024-04-08 18:47:31 +0800

    test(share): update cases

--- packages/share/test/lib/decorator/111.custom-decorator.helper.ts.test.ts
+++ packages/share/test/lib/decorator/111.custom-decorator.helper.ts.test.ts
@@ -1,18 +1,34 @@
 import assert from 'node:assert/strict'
 
+import { INJECT_CUSTOM_METHOD, saveClassMetadata } from '@midwayjs/core'
 import { fileShortPath } from '@waiting/shared-core'
 
 import {
-  methodHasDecorated,
   DecoratorMetaData,
+  customDecoratorFactory,
+  isMethodDecoratedWith,
+  methodHasDecorated,
 } from '##/index.js'
 
 
-describe(fileShortPath(import.meta.url), () => {
-  describe(`Should methodHasDecorated() work`, () => {
+const METHOD_KEY_Cacheable = 'decorator:method_key_cacheable_test'
 
+class Test {
+
+  @Cacheable({
+    cacheName: 'test',
+    ttl: 10,
+  })
+  _simple() {
+    return 'simple'
+  }
+
+}
+
+describe(fileShortPath(import.meta.url), () => {
+
+  describe(`Should methodHasDecorated() work`, () => {
     it(`empty`, () => {
-      const METHOD_KEY_Cacheable = 'cacheable_test'
       const methodName = 'test'
 
       const ret1 = methodHasDecorated(METHOD_KEY_Cacheable, methodName, [], true)
@@ -68,7 +84,57 @@ describe(fileShortPath(import.meta.url), () => {
       const ret6 = methodHasDecorated(key, methodName, [row], false)
       assert(ret6)
     })
+  })
+
+
+  describe(`Should isMethodDecoratedWith() work`, () => {
+    it(`empty`, () => {
+      const key = 'decorator:method_key_cacheable_test'
+      const methodName = '_simple'
+
+      const target = {}
+      const map = isMethodDecoratedWith(target, methodName, [key])
+      assert(map.size === 0)
+    })
+
+    it(`method`, () => {
+      const methodName = '_simple'
+      const row: DecoratorMetaData = {
+        key: METHOD_KEY_Cacheable,
+        metadata: { decoratedType: 'method' },
+        propertyName: methodName,
+        options: {
+          impl: true,
+        },
+      }
+
+      const map = isMethodDecoratedWith(Test, methodName, [METHOD_KEY_Cacheable])
+      assert(map.size)
+      let find = false
+      for (const key of map) {
+        if (key === METHOD_KEY_Cacheable) {
+          find = true
+          break
+        }
+      }
+      assert(find)
+    })
 
   })
+
 })
 
+
+function Cacheable(options?: Partial<CacheableArgs>) {
+  return customDecoratorFactory<CacheableArgs>({
+    decoratorArgs: options,
+    decoratorKey: METHOD_KEY_Cacheable,
+    enableClassDecorator: true,
+  })
+}
+
+
+interface CacheableArgs {
+  cacheName: string | undefined
+  ttl: number | undefined
+}
