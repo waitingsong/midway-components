fc091c558ceb47afb59d2aae04b5ae3004853d79
waiting <1661926154@qq.com>
2024-04-09 19:31:37 +0800

    refactor(share): reg-decorator-handler.ts with reg-decorator-handler.helper.ts

--- packages/share/src/lib/index.ts
+++ packages/share/src/lib/index.ts
@@ -2,6 +2,7 @@
 export * from './common.dto.js'
 export * from './decorator/reg-decorator.js'
 export * from './decorator/reg-decorator-handler.js'
+export { genDecoratorExecutorOptionsCommon } from './decorator/reg-decorator-handler.helper.js'
 export * from './decorator/custom-decorator.helper.js'
 export * from './decorator/custom-decorator.types.js'
 export * from './middleware.js'
