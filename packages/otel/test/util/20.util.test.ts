cb6cb4a3133c2eddf2cdbde74bfef8409077b3a5
waiting <1661926154@qq.com>
2024-04-10 00:20:08 +0800

    test(otel): update 20.util.test.ts
    truncateString()

--- packages/otel/test/util/20.util.test.ts
+++ packages/otel/test/util/20.util.test.ts
@@ -2,6 +2,7 @@ import assert from 'node:assert/strict'
 
 import { fileShortPath } from '@waiting/shared-core'
 
+import { truncateString } from '##/lib/util.js'
 import { apiPrefix, apiRoute } from '#@/fixtures/base-app/src/api-route.js'
 import { testConfig } from '#@/root.config.js'
 
@@ -19,6 +20,20 @@ describe(fileShortPath(import.meta.url), function () {
         .expect(200)
 
       assert(resp.text === 'OK')
+    })
+  })
+
+  describe('should truncateString() work', () => {
+    const str = 'abcdefg'
+
+    it('common', async () => {
+      const ret = truncateString(str)
+      assert(ret === str)
+    })
+
+    it('common', async () => {
+      const ret = truncateString(str, 1)
+      assert(ret === 'a... LENGTH: 7 bytes')
     })
   })
 
