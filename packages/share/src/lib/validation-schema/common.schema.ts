5cd0a06c521b4341a1836ae66e80e11e3219b288
waiting <1661926154@qq.com>
2024-08-16 16:43:17 +0800

    chore(share): format

--- packages/share/src/lib/validation-schema/common.schema.ts
+++ packages/share/src/lib/validation-schema/common.schema.ts
@@ -14,45 +14,38 @@ export const commonValidSchemas = {
   number: RuleType.number(),
   int: RuleType.number().integer(),
   /** 自然数 */
-  naturalNumber: RuleType.number().min(0)
-    .max(Number.MAX_SAFE_INTEGER),
+  naturalNumber: RuleType.number().min(0).max(Number.MAX_SAFE_INTEGER),
   /**
    * 大于等于0的正整数
    * @deprecated use naturalNumber instead
    */
-  nonNegativeInteger: RuleType.number().min(0)
-    .max(Number.MAX_SAFE_INTEGER),
-  /** 正整数 */
-  positiveInt: RuleType.number().min(1)
-    .max(Number.MAX_SAFE_INTEGER),
-  /** 负整数 */
-  negativeInt: RuleType.number().max(-1)
-    .max(-Number.MAX_SAFE_INTEGER),
+  nonNegativeInteger: RuleType.number().min(0).max(Number.MAX_SAFE_INTEGER),
 
-  id: RuleType.number().min(1)
-    .max(Number.MAX_SAFE_INTEGER),
-  name50: RuleType.string().trim()
-    .min(1)
-    .max(50),
-  bigintString: RuleType.string().trim()
-    .min(1)
-    .max(30),
+  /** 正整数 */
+  positiveInt: RuleType.number().min(1).max(Number.MAX_SAFE_INTEGER),
+
+  /** 负整数 */
+  negativeInt: RuleType.number().max(-1).max(-Number.MAX_SAFE_INTEGER),
+
+  id: RuleType.number().min(1).max(Number.MAX_SAFE_INTEGER),
+
+  name50: RuleType.string().trim().min(1).max(50),
+
+  bigintString: RuleType.string().trim().min(1).max(30),
+
   dateString: RuleType.date(),
+
   /** 雪花算法int结果字符串 */
-  snowflakeString: RuleType.string().trim()
-    .min(1)
-    .max(19),
+  snowflakeString: RuleType.string().trim().min(1).max(19),
+
   /** 分页查询每页面最大记录数 */
-  pageSizeMax: RuleType.number().min(1)
-    .max(1000),
+  pageSizeMax: RuleType.number().min(1).max(1000),
   /** 分页查询字段排序规则 */
   pageOrderBy: pageOrderByArraySchema,
-  stringMax100: RuleType.string().trim()
-    .min(1)
-    .max(100),
-  stringMax255: RuleType.string().trim()
-    .min(1)
-    .max(255),
+
+  stringMax100: RuleType.string().trim().min(1).max(100),
+  stringMax255: RuleType.string().trim().min(1).max(255),
+
   string: RuleType.string().trim(),
 }
 
