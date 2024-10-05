994dfda28b13bad5844c4f3ba3896513ba534a04
waiting <1661926154@qq.com>
2024-10-05 23:17:51 +0800

    chore(cache): lint fix

--- packages/cache/src/lib/types.ts
+++ packages/cache/src/lib/types.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/ban-types */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import type { CacheManagerOptions, CachingFactory } from '@midwayjs/cache-manager'
 import type { TraceService } from '@mwcp/otel'
